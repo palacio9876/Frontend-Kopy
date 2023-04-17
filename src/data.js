b9f2558641c8d999faa3eebf5d4bb1dbe684780c
Santiago <santiagosanchezluna@gmail.com>
2023-04-17 07:38:17 -0500

    carrito compras

---
+++ src/data.js
@@ -0,0 +1,238 @@
+export const data = [
+    {
+        id:1,
+        nameProduct:'Cafe oscuro',
+        price:2000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721659/kopy/cafe-oscuro_zlyaem.jpg',
+        quantity:1,
+    },
+
+    {
+        id:2,
+        nameProduct:'cafe con leche ',
+        price:2000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721658/kopy/cafe-leche_wv0caq.jpg',
+        quantity:1,
+    },
+
+    {
+        id:3,
+        nameProduct:'Cafe fusion',
+        price:3200,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721658/kopy/cafe-fusion_ekejg7.jpg',
+        quantity:1,
+    },
+
+    {
+        id:4,
+        nameProduct:'Cafe helado',
+        price:4500,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721658/kopy/cafe-helado_wgyu8h.webp',
+        quantity:1,
+    },
+
+    {
+        id:5,
+        nameProduct:'Cafe mango',
+        price:4000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721659/kopy/cafe-mango_nrjxp6.jpg',
+        quantity:1,
+    },
+
+    {
+        id:6,
+        nameProduct:'Capuchino',
+        price:3000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/capuchino_gtvlmz.jpg',
+        quantity:1,
+    },
+
+    {
+        id:7,
+        nameProduct:'Buñuelo',
+        price:2000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721657/kopy/bu%C3%B1uelo_cscvqj.jpg',
+        quantity:1,
+    },
+
+    {
+        id:8,
+        nameProduct:'Pan rosco',
+        price:1500,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721664/kopy/croasan_p1jcki.jpg',
+        quantity:1,
+    },
+
+    {
+        id:9,
+        nameProduct:'Galletas de fresa',
+        price:1000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721680/kopy/gaelletas-rellenas_gh9fac.webp',
+        quantity:1,
+    },
+
+    {
+        id:10,
+        nameProduct:'Galletas de leche',
+        price:1000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721662/kopy/gaelltas-azules_yeuzcr.jpg',
+        quantity:1,
+    },
+
+    {
+        id:11,
+        nameProduct:'Galletas chips chocolate',
+        price:1000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721662/kopy/galletas-chispas_lpt2we.jpg',
+        quantity:1,
+    },
+
+    {
+        id:12,
+        nameProduct:'Galletas de avena',
+        price:1000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721662/kopy/galletas_fv5kni.jpg',
+        quantity:1,
+    },
+
+    {
+        id:13,
+        nameProduct:'Pan de leche',
+        price:2000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721657/kopy/pan-casero_xrsdfm.jpg',
+        quantity:1,
+    },
+
+    {
+        id:14,
+        nameProduct:'Pan con queso',
+        price:2500,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721656/kopy/pan-de-queso_rzyjb5.jpg',
+        quantity:1,
+    },
+    {
+        id:15,
+        nameProduct:'Pan galleta',
+        price:3000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721657/kopy/pan-galleta_ododjk.jpg',
+        quantity:1,
+    },
+
+    {
+        id:16,
+        nameProduct:'Pan integral',
+        price:3000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721656/kopy/pan_tbwrsy.jpg',
+        quantity:1,
+    },
+
+    {
+        id:17,
+        nameProduct:'Milo frio',
+        price:1500,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721655/kopy/milo_voamzu.webp',
+        quantity:1,
+    },
+
+    {
+        id:18,
+        nameProduct:'Jugo de naranja',
+        price:1000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721663/kopy/jugo-naranja_v5qmln.webp',
+        quantity:1,
+    },
+    {
+        id:19,
+        nameProduct:'Jugo de mango',
+        price:1000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721662/kopy/jugo-mango_rskvos.jpg',
+        quantity:1,
+    },
+
+    {
+        id:20,
+        nameProduct:'Jugo de fresa',
+        price:1000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721656/kopy/JUGOS_NATURALES_cmkkn9.jpg',
+        quantity:1,
+    },
+
+    {
+        id:21,
+        nameProduct:'Chocolate',
+        price:1200,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/chocolate-sin_xe70pb.jpg',
+        quantity:1,
+    },
+
+    {
+        id:22,
+        nameProduct:'Chocolate con leche',
+        price:1500,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/chocolate_2_thkrdb.jpg',
+        quantity:1,
+    },
+
+    {
+        id:23,
+        nameProduct:'Cafe con leche',
+        price:1500,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/chocolate_mi0wv7.jpg',
+        quantity:1,
+    },
+    {
+        id:24,
+        nameProduct:'Calentado',
+        price:10000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/cale_fyjexs.webp',
+        quantity:1,
+    },
+
+    {
+        id:25,
+        nameProduct:'Calentado con huevo',
+        price:12000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/calentado_l9ohnj.jpg',
+        quantity:1,
+    },
+
+    {
+        id:26,
+        nameProduct:'Huevo con pan',
+        price:9000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721661/kopy/desayunos_caqpds.jpg',
+        quantity:1,
+    },
+
+    {
+        id:27,
+        nameProduct:'Huevo frito',
+        price:4000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721662/kopy/huevo-frito_ab4gno.jpg',
+        quantity:1,
+    },
+
+    {
+        id:28,
+        nameProduct:'Huevo revuelto',
+        price:5000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721662/kopy/huevos-revueltos_rc9mhv.webp',
+        quantity:1,
+    },
+
+    {
+        id:29,
+        nameProduct:'Huevo perico',
+        price:7000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721656/kopy/perico_fyfttb.jpg',
+        quantity:1,
+    },
+
+    {
+        id:30,
+        nameProduct:'Huevo con salchicha',
+        price:9000,
+        urlImage:'https://res.cloudinary.com/dyhfwq81d/image/upload/v1678829157/kopy/receta-desayuno-con-salchicha_goxw0r.jpg',
+        quantity:1,
+    },
+]
--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import { useState } from 'react';
 import { Footer } from "../layouts/Footer/Footer.jsx";
 import { Header } from "../layouts/Header/Header.jsx";
 import "../assets/css/Catalogo.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { ProductList } from "../Components/ProductList.jsx";
+import { Cart } from '../Components/Cart';
 
 export const Catalogo = () => {
+
+  const [allProducts, setAllProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
+
   AOS.init();
   return (
     <>
@@ -20,391 +27,25 @@ export const Catalogo = () => {
           <button className="chocolates btn-catalogo">Chocolates</button>
           <button className="desayunos btn-catalogo">Desayunos</button>
         </div>
+
+        <Cart 
+        allProducts={allProducts}
+				setAllProducts={setAllProducts}
+				total={total}
+				setTotal={setTotal}
+				countProducts={countProducts}
+				setCountProducts={setCountProducts}/>
+
         <div className="cards-container">
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="cafe">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721659/kopy/cafe-oscuro_zlyaem.jpg"
-              alt="Cafe oscuro"
-            />
-            <h2>Cafe oscuro</h2>
-            <p>Cafe termino oscuro</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="cafe">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721658/kopy/cafe-leche_wv0caq.jpg"
-              alt="Cafe con leche"
-            />
-            <h2>Cafe con leche</h2>
-            <p>Cafe, leche</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="cafe">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721658/kopy/cafe-fusion_ekejg7.jpg"
-              alt="Cafe fusion"
-            />
-            <h2>Cafe fusion</h2>
-            <p>Cafe fusion</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="cafe">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721658/kopy/cafe-helado_wgyu8h.webp"
-              alt="Cafe helado"
-            />
-            <h2>Cafe helado</h2>
-            <p>Cafe, helado</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="cafe">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721659/kopy/cafe-mango_nrjxp6.jpg"
-              alt="Cafe mango"
-            />
-            <h2>Cafe mango</h2>
-            <p>Cafe, mango</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="cafe">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/capuchino_gtvlmz.jpg"
-              alt="capuchino"
-            />
-            <h2>capuchino</h2>
-            <p>capuchino</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="panaderia">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721657/kopy/bu%C3%B1uelo_cscvqj.jpg"
-              alt="panaderia"
-            />
-            <h2>panaderia</h2>
-            <p>panaderia</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="panaderia">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721664/kopy/croasan_p1jcki.jpg"
-              alt="panaderia"
-            />
-            <h2>panaderia</h2>
-            <p>panaderia</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="panaderia">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721680/kopy/gaelletas-rellenas_gh9fac.webp"
-              alt="panaderia"
-            />
-            <h2>panaderia</h2>
-            <p>panaderia</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="panaderia">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721662/kopy/gaelltas-azules_yeuzcr.jpg"
-              alt="panaderia"
-            />
-            <h2>panaderia</h2>
-            <p>panaderia</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="panaderia">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721662/kopy/galletas-chispas_lpt2we.jpg"
-              alt="panaderia"
-            />
-            <h2>panaderia</h2>
-            <p>panaderia</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="panaderia">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721662/kopy/galletas_fv5kni.jpg"
-              alt="panaderia"
-            />
-            <h2>panaderia</h2>
-            <p>panaderia</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="panaderia">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721657/kopy/pan-casero_xrsdfm.jpg"
-              alt="panaderia"
-            />
-            <h2>panaderia</h2>
-            <p>panaderia</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="panaderia">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721656/kopy/pan-de-queso_rzyjb5.jpg"
-              alt="panaderia"
-            />
-            <h2>panaderia</h2>
-            <p>panaderia</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="panaderia">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721657/kopy/pan-galleta_ododjk.jpg"
-              alt="panaderia"
-            />
-            <h2>panaderia</h2>
-            <p>panaderia</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="panaderia">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721656/kopy/pan_tbwrsy.jpg"
-              alt="panaderia"
-            />
-            <h2>panaderia</h2>
-            <p>panaderia</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="jugos">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721655/kopy/milo_voamzu.webp"
-              alt="jugos"
-            />
-            <h2>jugos</h2>
-            <p>jugos</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="jugos">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721663/kopy/jugo-naranja_v5qmln.webp"
-              alt="jugos"
-            />
-            <h2>jugos</h2>
-            <p>jugos</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="jugos">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721662/kopy/jugo-mango_rskvos.jpg"
-              alt="jugos"
-            />
-            <h2>jugos</h2>
-            <p>jugos</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="jugos">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721656/kopy/JUGOS_NATURALES_cmkkn9.jpg"
-              alt="jugos"
-            />
-            <h2>jugos</h2>
-            <p>jugos</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="chocolates">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/chocolate-sin_xe70pb.jpg"
-              alt="chocolate"
-            />
-            <h2>Cafe oscuro</h2>
-            <p>Cafe termino oscuro</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="chocolates">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/chocolate_2_thkrdb.jpg"
-              alt="chocolate"
-            />
-            <h2>Cafe oscuro</h2>
-            <p>Cafe termino oscuro</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="chocolates">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/chocolate_mi0wv7.jpg"
-              alt="chocolate"
-            />
-            <h2>Cafe oscuro</h2>
-            <p>Cafe termino oscuro</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="chocolates">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/chocolate-sin_xe70pb.jpg"
-              alt="chocolate"
-            />
-            <h2>Cafe oscuro</h2>
-            <p>Cafe termino oscuro</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="chocolates">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/chocolate-sin_xe70pb.jpg"
-              alt="chocolate"
-            />
-            <h2>Cafe oscuro</h2>
-            <p>Cafe termino oscuro</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="desayunos">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/cale_fyjexs.webp"
-              alt="desayunos"
-            />
-            <h2>desayunos</h2>
-            <p>desayunos</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="desayunos">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721660/kopy/calentado_l9ohnj.jpg"
-              alt="desayunos"
-            />
-            <h2>desayunos</h2>
-            <p>desayunos</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="desayunos">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721661/kopy/desayunos_caqpds.jpg"
-              alt="desayunos"
-            />
-            <h2>desayunos</h2>
-            <p>desayunos</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="desayunos">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721662/kopy/huevo-frito_ab4gno.jpg"
-              alt="desayunos"
-            />
-            <h2>desayunos</h2>
-            <p>desayunos</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="desayunos">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721662/kopy/huevos-revueltos_rc9mhv.webp"
-              alt="desayunos"
-            />
-            <h2>desayunos</h2>
-            <p>desayunos</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="desayunos">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678721656/kopy/perico_fyfttb.jpg"
-              alt="desayunos"
-            />
-            <h2>desayunos</h2>
-            <p>desayunos</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
-          <div className="card-catalogo" data-aos="zoom-in" data-aos-duration="7000" data-catalogo="desayunos">
-            <img
-              src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1678829157/kopy/receta-desayuno-con-salchicha_goxw0r.jpg"
-              alt="desayunos"
-            />
-            <h2>desayunos</h2>
-            <p>desayunos</p>
-            <div className="price-btn">
-              <p>$2.000</p>
-              <button>comprar</button>
-            </div>
-          </div>
+
+          <ProductList
+            allProducts={allProducts}
+            setAllProducts={setAllProducts}
+            total={total}
+            setTotal={setTotal}
+            countProducts={countProducts}
+            setCountProducts={setCountProducts} />
+
         </div>
       </main>
       <Footer />
