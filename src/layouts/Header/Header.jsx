19cdf45a4705a91aafc6aa6db2833696a30c65f9
julianparra21 <julianparraos147@gmail.com>
2023-06-06 07:28:06 -0500

    Logo position ok

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -3,13 +3,12 @@ import { NavLink } from "react-router-dom";
     
   return (
     
-      <header className="bg-brown-kopy flex justify-between items-center px-[5%] py-2.5">
-        <div className="logo">
-          <img
-            className="w-20 h-w-20 rounded-full"
+      <header className="bg-brown-kopy h-40 flex justify-between items-center px-[5%]">
+        
+          <img className="w-40"
             src="https://res.cloudinary.com/dyhfwq81d/image/upload/v1685967057/Kopy_tfpadn.jpg"
           />
-        </div>
+      
         <nav>
           <ul className="flex items-center justify-center flex-wrap">
             <li className="mx-1.5 my-0 list-none uppercase hover:font-bold">
