e4cb1192c6a48319347ab925dcb2a618ce95c26b
Cristian Palacio <cristiancamilopala@gmail.com>
2023-06-13 07:52:36 -0500

    Buttons add admin

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -73,9 +73,11 @@ export const ProductList = ({
       const response = await axios.delete(
         // `http://localhost:3020/product/eliminar/${productId}`
         `https://kopy-backend.up.railway.app/product/eliminar/${productId}`
-        );
+      );
       if (response.status === 200) {
-        const updatedProducts = articles.filter((article) => article.id_producto !== productId);
+        const updatedProducts = articles.filter(
+          (article) => article.id_producto !== productId
+        );
         setArticles(updatedProducts);
         toast.success("Producto eliminado exitosamente", {
           position: "top-left",
@@ -106,11 +108,11 @@ export const ProductList = ({
   const editProduct = async (product) => {
     // Aquí deberías implementar la lógica para editar un producto
     try {
-     
       const response = await axios.put(
         // `http://localhost:3020/product/${product.id_producto}`
-        `https://kopy-backend.up.railway.app/product/${product.id_producto}`
-        , product);
+        `https://kopy-backend.up.railway.app/product/${product.id_producto}`,
+        product
+      );
       if (response.status === 200) {
         toast.success("Producto editado exitosamente", {
           position: "top-left",
@@ -138,83 +140,73 @@ export const ProductList = ({
     }
   };
 
- const onAddProduct = (article) => {
-
+  const onAddProduct = (article) => {
     if (allProducts.find((item) => item.id_producto === article.id_producto)) {
       const products = allProducts.map((item) =>
+        item.id_producto === article.id_producto
+          ? { ...item, cantidad_producto: item.cantidad_producto + 1 }
+          : item
+      );
 
-
-       item.id_producto === article.id_producto ? { ...item, cantidad_producto: item.cantidad_producto + 1 } 
-       : item
-     );
-     
-    //  setCountProducts(countProducts + product.cantidad_producto);
-     setTotal(total + article.precio * article.cantidad_producto);
-    setCountProducts(countProducts + article.cantidad_producto);
-     return setAllProducts([...products]);
-   }
+      //  setCountProducts(countProducts + product.cantidad_producto);
+      setTotal(total + article.precio * article.cantidad_producto);
+      setCountProducts(countProducts + article.cantidad_producto);
+      return setAllProducts([...products]);
+    }
 
     setTotal(total + article.precio * article.cantidad_producto);
     setCountProducts(countProducts + article.cantidad_producto);
     setAllProducts([...allProducts, article]);
-
-   
   };
 
   let rol = localStorage.getItem("rol");
   let rolAdmin = localStorage.getItem("rolAdmin");
 
   return (
-
-    <div className="container-items" >
-     {articles.map(article => (
-       <div className="item" key={article.id_producto}>
-           <figure>
-					<img
-						src={article.id_imagen} alt={article.nombre_producto}
-						
-					/>
-				</figure>
-        {rol === "rolAdmin" ? (
-                <>
-                  
-					<h2>{article.nombre_producto}</h2>
-					<p className="price">${article.precio}</p>
-          <button className="btn btn-danger rounded-[10px] mt-2 bg-orange-kopy h-14 w-72 hover:text-zinc-50 " onClick={() => editProduct(article)}>
-                    Editar
-                  </button>
-                    <br />
-                    
-                  <button className="btn btn-danger mt-2 rounded-[10px] bg-orange-kopy h-14 w-72 hover:text-zinc-50  " onClick={() => deleteProduct(article.id_producto)}>
-                    Eliminar
-                  </button>
-			
-                
-                </>
-              ) : (
-                <>
-                  <div className="info-product">
-					<h2>{article.nombre_producto}</h2>
-					<p className="price">${article.precio}</p>
-					<button onClick={() => onAddProduct(article)} onClickCapture={addCart} className="btn-add-cart">Añadir al carrito</button>
-				</div>
-                </>
-              )}
-
-				
-         
-       </div>
-
-
-
-       
-
-
-    ))}
-
-    
-
-
+    <div className="container-items">
+      {articles.map((article) => (
+        <div className="item" key={article.id_producto}>
+          <figure>
+            <img src={article.id_imagen} alt={article.nombre_producto} />
+          </figure>
+          {rol === "rolAdmin" ? (
+            <>
+              <h2 className="text-lg font-black ml-2">{article.nombre_producto}</h2>
+              <p className="text-lg ml-2">${article.precio}</p>
+              <div className="flex flex-col items-center mt-4">
+                {" "}
+                <button
+                  className="rounded-[10px]  bg-orange-kopy h-14 w-72 hover:text-zinc-50 "
+                  onClick={() => editProduct(article)}
+                >
+                  Editar
+                </button>
+                <br />
+                <button
+                  className="-mt-4 mb-4 rounded-[10px] bg-orange-kopy h-14 w-72 hover:text-zinc-50"
+                  onClick={() => deleteProduct(article.id_producto)}
+                >
+                  Eliminar
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="info-product">
+                <h2 className="text-lg font-black ml-2">{article.nombre_producto}</h2>
+                <p className="text-lg ml-2">${article.precio}</p>
+                <button
+                  onClick={() => onAddProduct(article)}
+                  onClickCapture={addCart}
+                  className="btn-add-cart"
+                >
+                  Añadir al carrito
+                </button>
+              </div>
+            </>
+          )}
+        </div>
+      ))}
     </div>
     /*
     <>
