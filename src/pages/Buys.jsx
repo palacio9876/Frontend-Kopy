512a756c2050dcf41fa9474f5d81aef3ec6aa6a2
Cristian Palacio <cristiancamilopala@gmail.com>
2023-06-21 01:01:17 -0500

    Stripe add

--- src/pages/Buys.jsx
+++ src/pages/Buys.jsx
@@ -1,112 +1,119 @@
-import React, { useState, useEffect } from "react";
-import { Footer } from "../layouts/Footer/Footer";
-import "../assets/css/buys.css";
-import axios from "axios";
-
-
-import { Header } from "../layouts/Header/Header";
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+  CardNumberElement,
+  CardCvcElement,
+  CardExpiryElement,
+} from "@stripe/react-stripe-js";
 import { HeaderCliente } from "../layouts/Header/HeaderCliente";
+import { Header } from "../layouts/Header/Header";
+import { Footer } from "../layouts/Footer/Footer";
 
-export const Buys = () => {
-  let rol = localStorage.getItem("rol");
+const stripePromise = loadStripe(
+  "pk_test_51MyDPuD7xwBecERveSCg9jpJLEtgGAwMkXjrx4THLT9QIc82suOAyC4caG8ZHbBj9oVovJGXW7xPfkqqTQcpHzDn00cTqVbT9n"
+);
 
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
 
- // const [data, setData] = useState({
-   // id: "",
-   // nombre: "",
-   // email: "",
-   // telefono: "",
-    //direccion: "",
-  //});
-  
-  
-
-  //useEffect(() => {
-
-   // (() => {
-     // axios
-      //  .get(
-          // `http://localhost:3020/user/profile`
-          //`https://kopy-backend.up.railway.app/user/profile`
-          //, {
-            //headers: {
-             // Authorization: token,
-
-           // },
-        //  })
-       // .then((res) => {
-         // console.log(res);
-          //console.log(res.data);
-          //setData(res.data);
-       // })
-       // .catch((error) => {
-          //console.log(error);
-       // });
-   // })();
- // }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+  };
 
   return (
+    <form onSubmit={handleSubmit}>
+      <div className="flex  flex-col">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Numero de tarjeta
+          </label>
+          <CardNumberElement className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Fecha de Vencimiento
+          </label>
+          <CardExpiryElement className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            CVV
+          </label>
+          <CardCvcElement className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+        </div>
+      </div>
+    </form>
+  );
+};
 
-    
+export const Buys = () => {
+  return (
     <>
-      {rol === null ? <Header /> : <HeaderCliente />}
-
-      <div className="cuadro">
-
-    <div className="caja1">
-      <h1>Informacion del cliente</h1>
-      <input className="datos" type="email" placeholder="  Correo electronio"></input>
-      <input className="datos" type="text" placeholder="  Nombre" required></input>
-      <input className="datos" type="text" placeholder="  Apellido" required></input>
-      <input className="datos" type="text" placeholder="  Identificacion" required></input>
-      <input className="datos" type="tel" placeholder="  Telefono" required></input>
-      <label className="checkbox">
-        <input type="checkbox" />
-        <span className="checkmark"></span>
-           Autorizo el uso de mis datos personales.
-      </label>
-    </div>
-
-    <div className="caja2">
-
-      <h2>Datos del envio</h2>
-
-      
-    <select className="municipios">
-    <option value="armenia">Armenia</option>
-    <option value="calarca">Calarcá</option>
-    <option value="circasia">Circasia</option>
-    <option value="la-tebaida">La Tebaida</option>
-    <option value="montenegro">Montenegro</option>
-    <option value="salento">Salento</option>
-    <option value="filandia">Filandia</option>
-    <option value="buenavista">Buenavista</option>
-    <option value="pijao">Pijao</option>
-    <option value="cordoba">Cordoba</option>
-    <option value="genova">Genova</option>
-    <option value="quimbaya">Quimbaya</option>
-    
-  </select>
-      <input className="datos" placeholder="Barrio" required></input>
-      <input className="datos" placeholder="Direccion" required></input>
-      </div>
-
-      <div className="caja3">
-      <h3>Datos de pago</h3>
-       
-          <p className="mediosDepago">Medios de pago</p>
-          <select className="medi">
-    <option value="tarjeta-credito">Tarjeta de crédito</option>
-    <option value="tarjeta-debito">Tarjeta de débito</option>
-    <option value="paypal">PayPal</option>
-    <option value="transferencia-bancaria">Transferencia bancaria</option>
-    <option value="Nequi">Nequi</option>
-  </select>
-
-  <button>Enviar</button>
-      </div> 
-      </div>
-     
+      <Header />
+      <Elements stripe={stripePromise}>
+        <div className="max-w-lg mx-auto">
+          <form className="bg-white shadow-md rounded  px-8 pt-6 pb-8 mb-4">
+            <div className="flex gap-7">
+              <div className="flex flex-col">
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Nombre
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="name"
+                    type="text"
+                    placeholder="Nombre"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    ID
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="id"
+                    type="text"
+                    placeholder="ID"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Correo Electrónico
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="email"
+                    type="email"
+                    placeholder="Correo Electrónico"
+                    required
+                  />
+                </div>
+              </div>
+              <CheckoutForm />
+            </div>
+            <div className="flex items-center justify-center mt-6">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Pagar
+              </button>
+            </div>
+          </form>
+        </div>
+      </Elements>
       <Footer />
     </>
   );
